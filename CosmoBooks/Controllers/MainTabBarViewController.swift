ab775c185de620c645a525a785d495dcb5638c6e
Wesley Osborne <wesleyosborne1@gmail.com>
2021-07-06 09:17:43 -0500

    Setup unlit extensions and cocoa pods

---
+++ CosmoBooks/Controllers/MainTabBarViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MaintTabBarViewController.swift
+//  CosmoBooks
+//
+//  Created by Wesley Osborne on 7/6/21.
+//
+
+import UIKit
+
+class MainTabBarViewController: UITabBarController, UITabBarControllerDelegate {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    
+    view.backgroundColor = .red
+  }
+  
+}
---
+++ CosmoBooks/Resources/UIKitExtensions.swift
@@ -0,0 +1,108 @@
+//
+//  UIKitExtensions.swift
+//  CosmoBooks
+//
+//  Created by Wesley Osborne on 7/6/21.
+//
+
+import UIKit
+
+extension UIColor {
+  
+  static func rgb(red: CGFloat, green: CGFloat, blue: CGFloat) -> UIColor {
+    return UIColor(red: red/255, green: green/255, blue: blue/255, alpha: 1)
+  }
+  
+  static func mainBlue() -> UIColor {
+    return UIColor.rgb(red: 17, green: 154, blue: 237)
+  }
+  
+}
+
+extension UIView {
+  
+  func fillSuperview() {
+    anchor(top: superview?.topAnchor, leading: superview?.leadingAnchor, bottom: superview?.bottomAnchor, trailing: superview?.trailingAnchor, centerX: nil, centerY: nil)
+  }
+  
+  func anchorSize(to view: UIView) {
+    widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+    heightAnchor.constraint(equalTo: view.heightAnchor).isActive = true
+  }
+  
+  func anchor(top: NSLayoutYAxisAnchor?, leading: NSLayoutXAxisAnchor?, bottom: NSLayoutYAxisAnchor?, trailing: NSLayoutXAxisAnchor?, centerX: NSLayoutXAxisAnchor? = nil, centerY: NSLayoutYAxisAnchor? = nil, padding: UIEdgeInsets = .zero, size: CGSize = .zero) {
+    
+    translatesAutoresizingMaskIntoConstraints = false
+    
+    if let top = top {
+      topAnchor.constraint(equalTo: top, constant: padding.top).isActive = true
+    }
+    
+    if let leading = leading {
+      leadingAnchor.constraint(equalTo: leading, constant: padding.left).isActive = true
+    }
+    
+    if let bottom = bottom {
+      bottomAnchor.constraint(equalTo: bottom, constant: -padding.bottom).isActive = true
+    }
+    
+    if let trailing = trailing {
+      trailingAnchor.constraint(equalTo: trailing, constant: -padding.right).isActive = true
+    }
+    
+    if let centerX = centerX {
+      centerXAnchor.constraint(equalTo: centerX).isActive = true
+    }
+    
+    if let centerY = centerY {
+      centerYAnchor.constraint(equalTo: centerY).isActive = true
+    }
+    
+    
+    if size.width != 0 {
+      widthAnchor.constraint(equalToConstant: size.width).isActive = true
+    }
+    
+    if size.height != 0 {
+      heightAnchor.constraint(equalToConstant: size.height).isActive = true
+    }
+    
+  }
+}
+
+extension Date {
+    func timeAgoDisplay() -> String {
+        let secondsAgo = Int(Date().timeIntervalSince(self))
+        
+        let minute = 60
+        let hour = 60 * minute
+        let day = 24 * hour
+        let week = 7 * day
+        let month = 4 * week
+        
+        let quotient: Int
+        let unit: String
+        if secondsAgo < minute {
+            quotient = secondsAgo
+            unit = "second"
+        } else if secondsAgo < hour {
+            quotient = secondsAgo / minute
+            unit = "min"
+        } else if secondsAgo < day {
+            quotient = secondsAgo / hour
+            unit = "hour"
+        } else if secondsAgo < week {
+            quotient = secondsAgo / day
+            unit = "day"
+        } else if secondsAgo < month {
+            quotient = secondsAgo / week
+            unit = "week"
+        } else {
+            quotient = secondsAgo / month
+            unit = "month"
+        }
+        
+        return "\(quotient) \(unit)\(quotient == 1 ? "" : "s") ago"
+        
+    }
+}
